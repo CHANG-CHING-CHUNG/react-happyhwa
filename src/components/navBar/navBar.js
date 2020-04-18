7e1a0b2928a676fc12534891a201928127e03566
CHANG-CHING-CHUNG <54635661+CHANG-CHING-CHUNG@users.noreply.github.com>
2020-04-18 11:32:11 +0800

    fixed top

--- src/components/navBar/navBar.js
+++ src/components/navBar/navBar.js
@@ -1,23 +1,45 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
-function NavBar() {
-  return (
-    <nav id="main-nav" className="navbar navbar-expand-sm" role="navigation">
-    <div id="nav-icon4" className="">
-      <span></span>
-      <span></span>
-      <span></span>
-    </div>
-    <div className="container justify-content-around">
-      <div className="navbar-nav ">
-        <NavLink exact to="/" activeClassName="active"  className="home nav-link nav-item">首頁</NavLink>
-        <NavLink to="/menu"  activeClassName="active" className="menu nav-link nav-item">美味菜單</NavLink>
-        <NavLink to="/delivery" activeClassName="active" className="delivery nav-link nav-item">訂餐外送</NavLink>
-      </div>
-    </div>
-  </nav>
-  );
+class NavBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state ={
+      onScroll:false,
+      topFixed:"top-fixed"
+    }
+  }
+
+  componentDidMount() {
+    window.onscroll = () => {
+      this.setState({ onScroll:true });
+    }
+  }
+
+
+
+  render() {
+    let className = "navbar navbar-expand-sm ";
+    if(this.state.onScroll && window.pageYOffset > 319.933) {
+      className += this.state.topFixed;
+    }
+    return (
+        <nav id="main-nav" className={className} role="navigation">
+        <div id="nav-icon4" className="">
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+        <div className="container justify-content-around">
+          <div className="navbar-nav ">
+            <NavLink exact to="/" activeClassName="active"  className="home nav-link nav-item">首頁</NavLink>
+            <NavLink to="/menu"  activeClassName="active" className="menu nav-link nav-item">美味菜單</NavLink>
+            <NavLink to="/delivery" activeClassName="active" className="delivery nav-link nav-item">訂餐外送</NavLink>
+          </div>
+        </div>
+      </nav>
+    )
+  }
 }
 
 export default NavBar;
