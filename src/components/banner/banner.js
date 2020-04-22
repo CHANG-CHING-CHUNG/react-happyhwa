d56825babd55939a87993b3857aec2c04de939c6
CHANG-CHING-CHUNG <54635661+CHANG-CHING-CHUNG@users.noreply.github.com>
2020-04-23 00:16:48 +0800

    Adding banner

---
+++ src/components/banner/banner.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import BannerImg from '../../images/banner/banner3.png';
+
+function Banner() {
+  return (
+    <div id="bannerBox">
+      <img id="bannerImg" src={BannerImg}/>
+    </div>
+  );
+}
+
+export default Banner;
--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,14 +1,51 @@
 import React from 'react';
+import Banner from '../banner/banner'
+
+function getDates() {
+  const get = new Date();
+  const getYear = get.getFullYear();
+  const getMonth = get.getMonth();
+  const getDate = get.getDate();
+
+  return `${getYear}-${getMonth+1}-${getDate}`;
+}
+
+const news = [
+  { id:1,
+    date:getDates(),
+    paragraph:"滿1200免運外送優惠~!"
+  }
+  ,
+  { id:2,
+    date:getDates(),
+    paragraph:"每星期日公休~!"
+  }
+  ,
+  { id:3,
+    date:getDates(),
+    paragraph:"美味好吃的控肉飯新上架~!"
+  }
+];
+
+function NewsLists() {
+  return (
+    news.map((post) => {
+      return (
+        <li className="newsLists" key={post.id}><strong>{post.date}</strong><p className="newsText">{post.paragraph}</p></li>
+      );
+    })
+  );
+}
+
 
 function Home() {
   return (
     <div id="home-page">
+      <Banner/>
       <div id="news">
        <h1 id="news-header">最新消息</h1>
        <ul id="news-wrapper">
-         <li>公休日公告~</li>
-         <li>優惠活動</li>
-         <li>不知道要寫什麼@0@</li>
+         <NewsLists/>
        </ul>
       </div>
     </div>
